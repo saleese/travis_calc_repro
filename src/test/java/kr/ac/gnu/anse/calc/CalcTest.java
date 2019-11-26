2e8dcd9ab46b727e1d2e2629adf5978a9d696b9b
S.A. Lee <saleese@gmail.com>
2019-11-26 23:00:03 +0900

    initial commit

---
+++ src/test/java/kr/ac/gnu/anse/calc/CalcTest.java
@@ -0,0 +1,17 @@
+package kr.ac.gnu.anse.calc;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class CalcTest 
+{
+	@Test
+	public void testAdd() {
+		Calc c = new Calc();
+		assertEquals(30, c.add(10, 20));
+	}
+	
+}
